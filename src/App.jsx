e3c7033413e013295751db697c094db6b20f6995
Brenden C Edwards <edwardsbrenden@gmail.com>
2025-06-15 00:13:06 -0500

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Header from './comps/header/header'
 import Content from './comps/body/content'
 import Footer from './comps/footer/footer'
 import './comps/nav/nav.css'
-
+import { BsFillHouseDoorFill } from 'react-icons/bs'
+import { BsFileCodeFill } from 'react-icons/bs'
+import { BsEnvelopeFill } from 'react-icons/bs'
+import { BsFilePdfFill } from 'react-icons/bs'
 function App() {
  
   return (
@@ -13,10 +16,10 @@ function App() {
     
     <div className='app'>
       <div className='nav'>
-        <button className='nav-button'>Home</button>
-        <button className='nav-button'>Projects</button>
-        <button className='nav-button'>Contact</button>
-        <button className='nav-button'>Resume</button>
+        <button className='nav-button'><BsFillHouseDoorFill /></button>
+        <button className='nav-button'><BsFileCodeFill /></button>
+        <button className='nav-button'><BsEnvelopeFill /></button>
+        <button className='nav-button'><BsFilePdfFill /></button>
       </div>
       <Header />
       <Content />
